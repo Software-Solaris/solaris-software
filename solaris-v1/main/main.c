c70576cdb3574f82d69524c243f25544606fcbed
Antonio <afrey23@esei.uvigo.es>
2026-01-09 16:43:30 +0000

    correccion main

--- solaris-v1/main/main.c
+++ solaris-v1/main/main.c
@@ -1,4 +1,4 @@
-#include "macros_esp.h"
+//#include "macros_esp.h"
 #include "spi.h"
 #include "core/macros.h"
 #include "icm20948.h"
@@ -7,25 +7,25 @@
 // #include "eventgroups.h"
 #include "../external/spp/services/logging/spp_log.h"
 
-static icm_data_t icm_data;
+//static icm_data_t icm_data;
 
 void app_main()
 {
    /** Initialization of the sensors */
-   retval_t ret = SPP_ERROR;
-   ret = IcmInit((void*)&icm_data);
-   ret = IcmConfig((void*)&icm_data);
+   retval_t ret = SPP_LOG_Init();
+   //ret = IcmInit((void*)&icm_data);
+   //ret = IcmConfig((void*)&icm_data);
    
    /** Initialization of the task for reading data from sensors */
-   void *p_icm_init;
-   void * p_task_storage;
-   void * p_buffer_eg;
-   void *p_event_group = NULL;
-   p_task_storage = SPP_OSAL_GetTaskStorage();
-   p_icm_init = SPP_OSAL_TaskCreate(IcmGetSensorsData, "ICM Read Sensors Task", STACK_SIZE, (void*)&icm_data, ICM_READ_SENSORS_PRIORITY, p_task_storage);
-   if (p_icm_init == NULL){
-      return;
-   }
+   //void *p_icm_init;
+   //void * p_task_storage;
+   //void * p_buffer_eg;
+   //void *p_event_group = NULL;
+   //p_task_storage = SPP_OSAL_GetTaskStorage();
+   //p_icm_init = SPP_OSAL_TaskCreate(IcmGetSensorsData, "ICM Read Sensors Task", STACK_SIZE, (void*)&icm_data, ICM_READ_SENSORS_PRIORITY, p_task_storage);
+   //if (p_icm_init == NULL){
+   //   return;
+   //}
    
    // p_buffer_eg = SPP_OSAL_GetEventGroupsBuffer();
    
@@ -36,9 +36,8 @@ void app_main()
 
 
    // 1. INICIALIZAR LOGS PRIMERO (para poder debuggear lo demás)
-   retval_t log_ret = SPP_LOG_Init();
-   if (log_ret != SPP_OK) {
-       printf("ERROR: Log system init failed: %d\n", log_ret);
+   if (ret != SPP_OK) {
+       printf("ERROR: Log system init failed: %d\n", ret);
    }
    
    SPP_LOG_SetLevel(SPP_LOG_VERBOSE);
