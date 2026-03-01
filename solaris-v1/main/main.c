3293ccc485eb72137cf36babb5d6aa66a9b865af
Oscar Barja <oscar.barja@alumnado.uvigo.gal>
2026-03-01 18:38:28 +0000

    proto main to test complete bmp flow

--- solaris-v1/main/main.c
+++ solaris-v1/main/main.c
@@ -1,7 +1,8 @@
-#include "core/returntypes.h"
 #include "core/core.h"
-#include "databank.h"
-#include "services/logging/spp_log.h"
+#include "core/returntypes.h"
+
+#include "spi.h"
+#include "spp_log.h"
 #include "osal/task.h"
 
 #include "services/databank/databank.h"
@@ -15,14 +16,13 @@ void app_main(void)
 {
     retval_t ret;
 
-    // 1) Init core (si aquí inicializas cosas comunes del proyecto, déjalo)
     Core_Init();
+    SPP_LOGI(TAG, "Boot");
 
-    // 2) Init databank explícito (para debuggear, luego se puede quitar porque el core ya lo inicializa)
     ret = SPP_DATABANK_init();
     if (ret != SPP_OK) {
-        SPP_LOGE(TAG, "SPP_DATABANK_init fallo");
-        while (1) { SPP_OSAL_TaskDelay(1000); }
+        SPP_LOGE(TAG, "Databank init failed");
+        for (;;) { SPP_OSAL_TaskDelay(1000); }
     }
 
     SPP_LOGI(TAG, "=== TEST DATABANK: START ===");
@@ -117,34 +117,20 @@ void app_main(void)
         while (1) { SPP_OSAL_TaskDelay(1000); }
     }
 
-    // 4) Cambiamos una variable random para luego chekear
-    p0->primary_header.version = 0xA0;
-    p1->primary_header.version = 0xA1;
-    p2->primary_header.version = 0xA2;
-
-    // Log de direcciones para comprobar que no devuelven el mismo puntero
-    SPP_LOGI(TAG, "p0=%p (version=0x%02X)", (void*)p0, p0->primary_header.version);
-    SPP_LOGI(TAG, "p1=%p (version=0x%02X)", (void*)p1, p1->primary_header.version);
-    SPP_LOGI(TAG, "p2=%p (version=0x%02X)", (void*)p2, p2->primary_header.version);
-
-    // 5) Devolvemos en orden (p0, p1, p2)
-    ret = SPP_DATABANK_returnPacket(p0);
-    if (ret != SPP_OK) SPP_LOGE(TAG, "return p0 fallo");
-
-    ret = SPP_DATABANK_returnPacket(p1);
-    if (ret != SPP_OK) SPP_LOGE(TAG, "return p1 fallo");
-
-    ret = SPP_DATABANK_returnPacket(p2);
-    if (ret != SPP_OK) SPP_LOGE(TAG, "return p2 fallo");
-
-    // 6) Volvemos a pedir 1 paquete
-    spp_packet_t *pX = SPP_DATABANK_getPacket();
-    if (!pX) {
-        SPP_LOGE(TAG, "Fallo pX despues del return");
-        while (1) { SPP_OSAL_TaskDelay(1000); }
+    // Keep handler order: 1st ICM dummy, 2nd BMP
+    void* p_spi_icm_dummy = SPP_HAL_SPI_GetHandler();
+    ret = SPP_HAL_SPI_DeviceInit(p_spi_icm_dummy);
+    if (ret != SPP_OK) {
+        SPP_LOGE(TAG, "SPI dev init ICM dummy failed");
+        for (;;) { SPP_OSAL_TaskDelay(1000); }
     }
 
-    SPP_LOGI(TAG, "pX=%p (esprado p2)", (void*)pX); // por ser LIFO
+    void* p_spi_bmp = SPP_HAL_SPI_GetHandler();
+    ret = SPP_HAL_SPI_DeviceInit(p_spi_bmp);
+    if (ret != SPP_OK) {
+        SPP_LOGE(TAG, "SPI dev init BMP failed");
+        for (;;) { SPP_OSAL_TaskDelay(1000); }
+    }
 
     void* p_spi_bmp = SPP_HAL_SPI_GetHandler();
     ret = SPP_HAL_SPI_DeviceInit(p_spi_bmp);
