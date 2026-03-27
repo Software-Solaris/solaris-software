5d4ba75e09e1625af1ce3c5002aca2e15870c9f0
Silent Whisper <132281492+secureshadow@users.noreply.github.com>
2026-03-27 18:51:58 +0100

    Clean up of build folders

--- solaris-v1/components/icm_driver/tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Documents/solaris-software/solaris-v1/components/icm_driver/tests
-# Build directory: /home/user/Documents/solaris-software/solaris-v1/components/icm_driver/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(icm20948_tests "/home/user/Documents/solaris-software/solaris-v1/components/icm_driver/tests/build/test_icm20948")
-set_tests_properties(icm20948_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/user/Documents/solaris-software/solaris-v1/components/icm_driver/tests/CMakeLists.txt;22;add_test;/home/user/Documents/solaris-software/solaris-v1/components/icm_driver/tests/CMakeLists.txt;0;")
