402f4cedf09c9ce849f6e57d38340b8a8968a49c
Silent Whisper <132281492+secureshadow@users.noreply.github.com>
2026-06-07 16:35:13 +0200

    Cleaned up main.c

---
+++ solaris-v2/main/init.c
@@ -0,0 +1,52 @@
+/**
+ * @file init.c
+ * @brief System initialisation sequence for the Solaris firmware.
+ */
+
+#include "init.h"
+
+#include "spp/spp.h"
+#include "spp/hal/hal.h"
+#include "spp/ports/hal/esp32/halEsp32.h"
+#include "spp/services/bmp390/bmp390.h"
+#include "spp/services/icm20948/icm20948.h"
+#include "spp/services/pubsub/pubsub.h"
+#include "spp/services/service.h"
+
+/* ----------------------------------------------------------------
+ * PUBLIC FUNCTIONS
+ * ---------------------------------------------------------------- */
+
+SPP_RetVal_t SPP_MAIN_init(void)
+{
+    SPP_RetVal_t ret = SPP_HAL_init(SPP_PORTS_ESP32S3_getHalPorts());
+    if (ret != K_SPP_OK)
+    {
+        return ret;
+    }
+
+    const SPP_SERVICE_ProducerContract_t *p_bmpProducerContract = SPP_SERVICES_BMP390_getProducerContract();
+    if (p_bmpProducerContract == NULL)
+    {
+        return K_SPP_ERROR_NULL_POINTER;
+    }
+    ret = SPP_SERVICES_PUBSUB_registerProducer(p_bmpProducerContract);
+    if (ret != K_SPP_OK)
+    {
+        return ret;
+    }
+
+    const SPP_SERVICE_ProducerContract_t *p_icmProducerContract = SPP_SERVICES_ICM20948_getProducerContract();
+    if (p_icmProducerContract == NULL)
+    {
+        return K_SPP_ERROR_NULL_POINTER;
+    }
+    ret = SPP_SERVICES_PUBSUB_registerProducer(p_icmProducerContract);
+    if (ret != K_SPP_OK)
+    {
+        return ret;
+    }
+
+    ret = SPP_CORE_init();
+    return ret;
+}
--- solaris-v2/main/main.c
+++ solaris-v2/main/main.c
@@ -1,59 +1,13 @@
 #include "spp/core/returnTypes.h"
-#include "spp/spp.h"
-#include "spp/services/bmp390/bmp390.h"
-#include "spp/services/icm20948/icm20948.h"
 #include "spp/services/pubsub/pubsub.h"
-#include "spp/services/service.h"
-#include "spp/hal/hal.h"
-#include "spp/ports/hal/esp32/halEsp32.h"
+#include "init.h"
 
 
 void app_main(void)
 {
-    /*  Set the HAL ports to the platform being used (ESP32S3)
-        and do all the init required to get the core stack running */
-    SPP_RetVal_t ret = SPP_HAL_init(SPP_PORTS_ESP32S3_getHalPorts());
+    SPP_RetVal_t ret = SPP_MAIN_init();
     if (ret != K_SPP_OK)
     {
-        /* Some error happened doing the HAL initialization */
-        return;
-    }
-
-    // Register the BMP390 service
-    const SPP_SERVICE_ProducerContract_t *p_bmpProducerContract = SPP_SERVICES_BMP390_getProducerContract();
-    if (p_bmpProducerContract == NULL)
-    {
-        return;
-    }
-    else
-    {
-        ret = SPP_SERVICES_PUBSUB_registerProducer(p_bmpProducerContract);
-        if (ret != K_SPP_OK)
-        {
-            return;
-        }
-    }
-
-    //Register ICM20948 producer
-    const SPP_SERVICE_ProducerContract_t *p_icmProducerContract = SPP_SERVICES_ICM20948_getProducerContract();
-    if (p_icmProducerContract == NULL)
-    {
-        return;
-    }
-    else
-    {
-        ret = SPP_SERVICES_PUBSUB_registerProducer(p_icmProducerContract);
-        if (ret != K_SPP_OK)
-        {
-            return;
-        }
-    }
-
-    /* Init the SPP core software stack */
-    ret = SPP_CORE_init();
-    if (ret != K_SPP_OK)
-    {
-        /* Some error happened doing the core initialization */
         return;
     }
 
