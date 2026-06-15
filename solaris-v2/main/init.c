8a0bbf39adfd94ff12931287ff81e94b7330aef3
Oscar Barja <oscar.barja@alumnado.uvigo.gal>
2026-06-15 10:45:13 +0000

    init.c consumers example of use

--- solaris-v2/main/init.c
+++ solaris-v2/main/init.c
@@ -23,9 +23,8 @@ SPP_RetVal_t SPP_MAIN_init(void)
     SPP_Kpid_t bmp390Kpid = {0};
     SPP_Kpid_t icm20948Kpid = {0};
 
-    SPP_Kpid_t sensorSubscription = {0};
-
-    sensorSubscription.value = bmp390Kpid.value | icm20948Kpid.value;
+    SPP_Kpid_t sdSubscription = {0};
+    SPP_Kpid_t e22mbl01Subscription = {0};
 
     SPP_RetVal_t ret = SPP_HAL_init(SPP_PORTS_ESP32S3_getHalPorts());
     if (ret != K_SPP_OK)
@@ -77,6 +76,26 @@ SPP_RetVal_t SPP_MAIN_init(void)
         return ret;
     }
 
+    // Example of use with subscriptions to differents producers
+    sdSubscription.value = bmp390Kpid.value | icm20948Kpid.value;
+    e22mbl01Subscription.value = icm20948Kpid.value;
+
+    // TO-DO: add this function to consumer drivers
+    // const SPP_SERVICE_ConsumerContract_t *p_sdConsumerContract = SPP_SERVICES_DATALOGGER_getConsumerContract();
+    const SPP_SERVICE_ConsumerContract_t *p_sdConsumerContract = NULL;
+    ret = SPP_SERVICES_PUBSUB_registerConsumer(p_sdConsumerContract, sdSubscription);
+    if (ret != K_SPP_OK)
+    {
+        return ret;
+    }
+
+    // const SPP_SERVICE_ConsumerContract_t *p_e22mbl01ConsumerContract = SPP_SERVICES_E22MBL01_getConsumerContract();
+    const SPP_SERVICE_ConsumerContract_t *p_e22mbl01ConsumerContract = NULL;
+    ret = SPP_SERVICES_PUBSUB_registerConsumer(p_e22mbl01ConsumerContract, e22mbl01Subscription);
+    if (ret != K_SPP_OK)
+    {
+        return ret;
+    }
 
     ret = SPP_CORE_init();
     return ret;
